c79f093613b118218f66ca04e6f844b329f7f47f
Jerry Wiltse <jerrywiltse@gmail.com>
2018-06-01 16:15:56 -0400

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
+import os
+
+
+class EnetConan(ConanFile):
+    name = "enet"
+    version = "1.3.13"
+    description = "ENet reliable UDP networking library"
+    url = "https://github.com/bincrafters/conan-enet"
+    homepage = "https://github.com/lsalzman/enet"
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    source_subfolder = "source_subfolder"
+    
+    def source(self):
+        source_url = "http://enet.bespin.org/download"
+        tools.get("{0}/{1}-{2}.tar.gz".format(source_url, self.name, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self.source_subfolder)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
+        include_folder = os.path.join(self.source_subfolder, "include")
+        self.copy(pattern="*", dst="include", src=include_folder)
+        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.compiler == "Visual Studio":
+            self.cpp_info.libs.append("ws2_32")
+            self.cpp_info.libs.append("winmm")
