32db711f95e68cc172b9e2607faaf2172e47a6bb
Croydon <git@cr0ydon.com>
2019-10-10 09:32:16 +0200

    Install experimental bincrafters-package-tools

--- .ci/install.sh
+++ .ci/install.sh
@@ -20,6 +20,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 pip install conan --upgrade
-pip install conan_package_tools bincrafters_package_tools
+pip install -i https://test.pypi.org/simple/ bincrafters-package-tools
 
 conan user
